76d5204798d488bcc77ac2d803a42693324556b3
Ragupathi <ragu3610@gmail.com>
2018-10-17 16:13:30 +0530

    index.php file alignment

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ Author URI:   https://developer.wordpress.org/
 // use Kunnu\Dropbox\DropboxFile;
 // use Kunnu\Dropbox\Exceptions\DropboxClientException;
 class DropboxUpload{
-	public $folder =  WP_CONTENT_DIR.'/to_upload';
+	// public $folder =  WP_CONTENT_DIR.'/to_upload';
 	public function __construct(){
 		$this->initial();
 	}
@@ -44,56 +44,6 @@ class DropboxUpload{
 		add_filter('shot-code',array($this,'shot_code_callback'),10,1);
 		add_action('init',array($this,'store_form_data'));
 	}
-
-	// public function edit_short_code(){
-	// 	global $wpdb;
-	// 	$table_name = $this->db_prefix().'custome_form';
-	// 	$edit_short_code = $wpdb->get_results("SELECT * FROM $table_name WHERE id ='".$_POST['short_code_id']."'",ARRAY_A)[0];
-	// 	if(!empty($edit_short_code)){
-	// 		include PLUGIN_DIR_PATH.'view/edit_short_code.php';
-	// 		wp_die();
-	// 	}
-		
-	// }
-
-	// public function update_short_code_details(){
-	// 		 global $wpdb;
-	// 	 $shot_code = json_decode(stripslashes($_POST['update_short_code_details']));
-	// 	 $shortcode_name = $shot_code->shortcode_name;
-	// 	 $shortcode_id = $shot_code->short_code_id;
-	// 	 $new_label_data['label_name'] = $shot_code->label_name;
-	// 	 $new_field_data['field_name'] = $shot_code->field_name;
-	// 	 foreach ($new_label_data as $key => $value) {
-	// 	 	foreach ($value as $key => $label_value) {
-	// 	 		$newlabel_value[] = $label_value; 
-	// 	 	}
-	// 	 }
-	// 	  foreach ($new_field_data as $key => $value) {
-	// 	 	foreach ($value as $key => $field_value) {
-	// 	 		$newfield_value[] = $field_value; 
-	// 	 	}
-	// 	 }
-		 
-	// 	$form_array = serialize(array_combine(array_filter($newlabel_value),array_filter($newfield_value)));
-	// 	if(!empty($form_array) && !empty($shortcode_name)){
-	// 		$column_values = array('form_id'=>$shortcode_name,'string'=>$form_array);
-	// 		$where = array('id'=>$shortcode_id);
-	// 		$table_name = $this->db_prefix().'custome_form';
-	// 		$update = $wpdb->update($table_name,$column_values,$where);
-	// 		if($update){
-	// 			echo json_encode(array('status'=>'1'));
-	// 			wp_die();
-	// 		}else{
-	// 			echo json_encode(array('status'=>'0'));
-	// 			wp_die();
-	// 		}
-	// 	}else{
-	// 		echo json_encode(array('status'=>'0'));
-	// 			wp_die();
-	// 	}
-		
-	// }
-
 	public function delete_short_code(){
 		global $wpdb;
 		$table_name = $this->db_prefix().'custome_form';
@@ -108,39 +58,13 @@ class DropboxUpload{
 		}
 		
 	}
-
-	// public function credentials(){
-	// 	unset($_POST['action']);
-	// 	global $wpdb;
-	// 	function example_callback($string){
-	// 		return array('app_key' =>'2qc3n2l4gqwb7uc','app_secret'=>'v7k9uce68lf85ch','access_token'=>'pUmE_WvIvEAAAAAAAAABHrijcymlbi0ed8vh5m3U5ua9pQhgGVVLkv23YlZAvDeD');
-	// 	}
-	// 	add_filter( 'example_filter', 'example_callback',10,1);
-	// 	$sample = apply_filters( 'example_filter', $_POST);
-	// 	$table_name = $this->db_prefix().'dropbox_details';
-	// 	$insert = $wpdb->insert($table_name,$sample);
-	// 	if($insert){
-	// 		echo json_encode(array('status'=>'1'));
-	// 		wp_die();
-	// 	}else{
-	// 		echo json_encode(array('status'=>'0'));
-	// 		wp_die();
-	// 	}
-	// 	// die();
-	// }
-	// public function dropbox_sdk(){
-	// 	$dir  = $this->folder;
-	// 	$option =$this->folder;
-	// 	return $this->recursiveScan($dir,$option); 
-	// }
-
 	public function add_new_shotcode(){
-		 global $wpdb;
-		 $table_name = $this->db_prefix().'custome_form';
-		 $shot_code = json_decode(stripslashes($_POST['shot_code']));
-		 $form_array = serialize($shot_code);
-		 // $shortcode_name = $shot_code->shortcode_name;
-		 $shortcode_name = str_replace(" ", "-",$shot_code->shortcode_name);
+		global $wpdb;
+		$table_name = $this->db_prefix().'custome_form';
+		$shot_code = json_decode(stripslashes($_POST['shot_code']));
+		$form_array = serialize($shot_code);
+		// $shortcode_name = $shot_code->shortcode_name;
+		$shortcode_name = str_replace(" ", "-",$shot_code->shortcode_name);
 		$column_values = array('form_id'=>$shortcode_name,'string'=>$form_array);
 		$shotcode = $wpdb->insert($table_name,$column_values);
 		if($shotcode){
@@ -150,30 +74,7 @@ class DropboxUpload{
 			echo json_encode(array('status'=>'0'));
 			wp_die();
 		}
-		 
 	}
-
-	// public function recursiveScan($dir,$option){
-	// 	global $wpdb;
-	// 	$table_name = $this->db_prefix().'dropbox_details';
-	// 	$data = $wpdb->get_results("SELECT * FROM $table_name",ARRAY_A)[0];
-	// 	$app = new DropboxApp($data['app_key'],$data['app_secret'],$data['access_token']);
-	// 	$dropbox = new Dropbox($app);
-	// 	$tree = glob(rtrim($dir, '/') . '/*');
-	// 	if (is_array($tree)) {
-	// 		foreach($tree as $file) {
-	// 			if(is_file($file)){
-	// 				$folder_file_name = str_replace($option, '', $file);
-	// 				$file = new DropboxFile($file);
-	// 				$uploadedFile = $dropbox->upload($file,$folder_file_name);
-	// 				if($uploadedFile){
-	// 				}
-	// 			}elseif (is_dir($file)) {
-	// 				$this->recursiveScan($file,$option);
-	// 			}
-	// 		}
-	// 	}
-	// }
 	public function menu(){
 		add_menu_page('Form Page','Form','manage_options','create-form');
 		add_submenu_page('create-form','Create Form','Add Shot Code','manage_options','create-form',array($this,'custome_form'));
@@ -217,43 +118,42 @@ class DropboxUpload{
 		include PLUGIN_DIR_PATH.'view/shot_code_values.php';
 	}
 	public function apply_filter(){
-	global $wpdb;
-	$table_name  = $this->db_prefix()."custome_form";
-	$value =  $wpdb->get_results("SELECT * FROM $table_name ",ARRAY_A);
-		if(!empty($value)){
-			$apply_filter = apply_filters('shot-code',$value);
-			foreach ($apply_filter as  $shortcode_name => $shortcode_value) {
-				unset($shortcode_value['shortcode_name']);
-				add_shortcode($shortcode_name,function() use ($shortcode_value){
-					foreach ($shortcode_value as $key => $new_value) {
-						foreach ($new_value as $key => $value) {
-							if($value['field_type'] =="radio"){
-								$result.=$value['label_name']."<br />";
-							foreach ($value['radio_button'] as $key => $radio_value) {
-									$result .= "$radio_value:<input type='".$value['field_type']."' value='".$radio_value."' name='".str_replace(' ','',$value['label_name'])."'><br>";
-								}
-
-							}
-							if(($value['field_type'] !="radio")&&($value['field_type'] !='checkbox')){
-								$label_name = $value['label_name'];
-								$type = $value['field_type'];
-								$result .= "$label_name:<input type='".$type."' name='".str_replace(' ','',$label_name)."'><br>";
-							}
-							if($value['field_type']=='checkbox'){
-								$result.=$value['label_name']."<br />";
-								foreach ($value['checkbox'] as $key => $checkbox_name) {
-									$result .= "$checkbox_name<input type='".$value['field_type']."' value='".$checkbox_name."' name='".str_replace(' ','',$value['label_name'])."[]'><br>";
-								}
-							}
-						}
-					}
-					echo "<form action='#' method='POST' id='form_data' enctype='multipart/form-data'>
-						$result.<input type='submit' id='store_form_value' name='register'>
-						</form>";
-				});
-			}
-		}
-	}
+        global $wpdb;
+        $table_name  = $this->db_prefix()."custome_form";
+        $value =  $wpdb->get_results("SELECT * FROM $table_name ",ARRAY_A);
+        if(!empty($value)){
+            $apply_filter = apply_filters('shot-code',$value);
+            foreach ($apply_filter as  $shortcode_name => $shortcode_value) {
+                unset($shortcode_value['shortcode_name']);
+                add_shortcode($shortcode_name,function() use ($shortcode_value){
+                foreach ($shortcode_value as $key => $new_value) {
+                    foreach ($new_value as $key => $value) {
+                    if($value['field_type'] =="radio"){
+                    $result.=$value['label_name']."<br />";
+                    foreach ($value['radio_button'] as $key => $radio_value) {
+                    $result .= "$radio_value:<input type='".$value['field_type']."' value='".$radio_value."' name='".str_replace(' ','',$value['label_name'])."'><br>";
+                    }
+                    }
+                    if(($value['field_type'] !="radio")&&($value['field_type'] !='checkbox')){
+                    $label_name = $value['label_name'];
+                    $type = $value['field_type'];
+                    $result .= "$label_name:<input type='".$type."' name='".str_replace(' ','',$label_name)."'><br>";
+                    }
+                    if($value['field_type']=='checkbox'){
+                    $result.=$value['label_name']."<br />";
+                    foreach ($value['checkbox'] as $key => $checkbox_name) {
+                    $result .= "$checkbox_name<input type='".$value['field_type']."' value='".$checkbox_name."' name='".str_replace(' ','',$value['label_name'])."[]'><br>";
+                    }
+                    }
+                    }
+                }
+                echo "<form action='#' method='POST' id='form_data' enctype='multipart/form-data'>
+                $result.<input type='submit' id='store_form_value' name='register'>
+                </form>";
+                });
+            }
+        }
+    }
 	public function store_form_data(){
 		if(isset($_POST['register'])){
 			if(!empty($_FILES)){
@@ -327,10 +227,102 @@ class DropboxUpload{
 		dbDelta( $sql2 );
 	}
 		// ABSPATH is current project Directory dropbox-wordpress
+	// public function edit_short_code(){
+	// 	global $wpdb;
+	// 	$table_name = $this->db_prefix().'custome_form';
+	// 	$edit_short_code = $wpdb->get_results("SELECT * FROM $table_name WHERE id ='".$_POST['short_code_id']."'",ARRAY_A)[0];
+	// 	if(!empty($edit_short_code)){
+	// 		include PLUGIN_DIR_PATH.'view/edit_short_code.php';
+	// 		wp_die();
+	// 	}
+		
+	// }
+
+	// public function update_short_code_details(){
+	// 		 global $wpdb;
+	// 	 $shot_code = json_decode(stripslashes($_POST['update_short_code_details']));
+	// 	 $shortcode_name = $shot_code->shortcode_name;
+	// 	 $shortcode_id = $shot_code->short_code_id;
+	// 	 $new_label_data['label_name'] = $shot_code->label_name;
+	// 	 $new_field_data['field_name'] = $shot_code->field_name;
+	// 	 foreach ($new_label_data as $key => $value) {
+	// 	 	foreach ($value as $key => $label_value) {
+	// 	 		$newlabel_value[] = $label_value; 
+	// 	 	}
+	// 	 }
+	// 	  foreach ($new_field_data as $key => $value) {
+	// 	 	foreach ($value as $key => $field_value) {
+	// 	 		$newfield_value[] = $field_value; 
+	// 	 	}
+	// 	 }
+		 
+	// 	$form_array = serialize(array_combine(array_filter($newlabel_value),array_filter($newfield_value)));
+	// 	if(!empty($form_array) && !empty($shortcode_name)){
+	// 		$column_values = array('form_id'=>$shortcode_name,'string'=>$form_array);
+	// 		$where = array('id'=>$shortcode_id);
+	// 		$table_name = $this->db_prefix().'custome_form';
+	// 		$update = $wpdb->update($table_name,$column_values,$where);
+	// 		if($update){
+	// 			echo json_encode(array('status'=>'1'));
+	// 			wp_die();
+	// 		}else{
+	// 			echo json_encode(array('status'=>'0'));
+	// 			wp_die();
+	// 		}
+	// 	}else{
+	// 		echo json_encode(array('status'=>'0'));
+	// 			wp_die();
+	// 	}
+		
+	// }
+
+		// public function credentials(){
+	// 	unset($_POST['action']);
+	// 	global $wpdb;
+	// 	function example_callback($string){
+	// 		return array('app_key' =>'2qc3n2l4gqwb7uc','app_secret'=>'v7k9uce68lf85ch','access_token'=>'pUmE_WvIvEAAAAAAAAABHrijcymlbi0ed8vh5m3U5ua9pQhgGVVLkv23YlZAvDeD');
+	// 	}
+	// 	add_filter( 'example_filter', 'example_callback',10,1);
+	// 	$sample = apply_filters( 'example_filter', $_POST);
+	// 	$table_name = $this->db_prefix().'dropbox_details';
+	// 	$insert = $wpdb->insert($table_name,$sample);
+	// 	if($insert){
+	// 		echo json_encode(array('status'=>'1'));
+	// 		wp_die();
+	// 	}else{
+	// 		echo json_encode(array('status'=>'0'));
+	// 		wp_die();
+	// 	}
+	// 	// die();
+	// }
+	// public function dropbox_sdk(){
+	// 	$dir  = $this->folder;
+	// 	$option =$this->folder;
+	// 	return $this->recursiveScan($dir,$option); 
+	// }
+
+		// public function recursiveScan($dir,$option){
+	// 	global $wpdb;
+	// 	$table_name = $this->db_prefix().'dropbox_details';
+	// 	$data = $wpdb->get_results("SELECT * FROM $table_name",ARRAY_A)[0];
+	// 	$app = new DropboxApp($data['app_key'],$data['app_secret'],$data['access_token']);
+	// 	$dropbox = new Dropbox($app);
+	// 	$tree = glob(rtrim($dir, '/') . '/*');
+	// 	if (is_array($tree)) {
+	// 		foreach($tree as $file) {
+	// 			if(is_file($file)){
+	// 				$folder_file_name = str_replace($option, '', $file);
+	// 				$file = new DropboxFile($file);
+	// 				$uploadedFile = $dropbox->upload($file,$folder_file_name);
+	// 				if($uploadedFile){
+	// 				}
+	// 			}elseif (is_dir($file)) {
+	// 				$this->recursiveScan($file,$option);
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+
 }
 $obj = new DropboxUpload();
-
-
-// $data = get_site_transient('update_plugins');
-// print_r($data);
-// exit();
