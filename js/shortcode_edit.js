480ba3b60c7a455023382406fbdde1b632dffd01
Ragupathi <ragu3610@gmail.com>
2018-10-18 18:14:19 +0530

    error i value

--- js/shortcode_edit.js
+++ js/shortcode_edit.js
@@ -11,9 +11,19 @@ jQuery(document).ready(function(){
 			short_code_id:short_code_id,
 		},
 		success:function(data){
-			console.log(data);
+			// console.log(data);
 			jQuery(".show-form-hide").hide();
 			jQuery(".edit-form").html(data);
+			jQuery("#wp_add_more").click(function(e){
+				e.preventDefault();
+				i = jQuery(this).attr("iValue");
+				console.log(i);
+				var append_add_more = '<div class="row wp_main_short_code"><div class="form-group col-md-5"><label for="text">Label Name</label><input type="text" class="form-control label_name "   id="text" placeholder="Enter text" ></div><div class="form-group  col-md-5"><label for="exampleSelect1">Field Type</label><select class="form-control wp_shot_add_shot_code_option field_type" style="height:34px;" id="'+i+'"><option value="text" class="wp_text">Text</option><option class="wp_email" value="email">Email</option><option class="wp_password" value="password">Password</option><option class="wp_checkbox" value="checkbox">Check Box</option><option class="wp_radio" value="radio">Radio Button</option><option class="" value="file">File Upload</option></select></div><div class="col-md-2"><button type="button" class="wp_main_short_code_remove btn btn-danger">Delete</button></div><div class="wp_append_checkbox'+i+'"></div><div class="wp_append_radio'+i+'"></div>';
+			jQuery(".wp_single_set_short_code").append(append_add_more);
+		i++
+
+				// console.log("Hello World");
+			});
 			// jQuery("#add_add_more_field").click(function(){
 			// 	jQuery(".custome_form").append('<div class="single-short-code"><div class="form-group col-md-5"><label for="text">Label Name</label><input type="text" class="form-control label_name" id="text" placeholder="Enter text" name="label[]"></div><div class="form-group col-md-5"><label for="pwd">Field Name</label><input type="text" class="form-control field_name" placeholder="text,password" name="field[]"></div><div class="form-group col-md-2"><input class="btn btn-danger remove-short-code" style="margin-top:25px" type="button" value="Delete"></div></div>');
 			// });
--- view/edit_short_code.php
+++ view/edit_short_code.php
@@ -2,8 +2,10 @@
 	
 	$shortcode_form_input = $edit_short_code['string'];
 	$data = json_decode(json_encode(unserialize($shortcode_form_input)),true);
+	$i = 0;
 	foreach($data['form_data'] as $key =>$value){
 		$add_more_db_data .='<div class="row wp_main_short_code"><div class="form-group col-md-5"><label for="text">Label Name</label><input type="text" class="form-control label_name " value='.$value['label_name'].'  id="text" placeholder="Enter text" ></div><div class="form-group  col-md-5"><label for="exampleSelect1">Field Type</label><select class="form-control wp_shot_add_shot_code_option field_type"   style="height:34px;"><option value="text" '.($value['field_type'] =='text' ? 'selected' : '').'  class="wp_text">Text</option><option class="wp_email" value="email" '.($value['field_type'] =='email' ? 'selected' : '').' >Email</option><option class="wp_password" value="password" '.($value['field_type'] =='password' ? 'selected' : '').'>Password</option><option class="wp_checkbox" value="checkbox" '.($value['field_type'] =='checkbox' ? 'selected' : '').'>Check Box</option><option class="wp_radio" value="radio" '.($value['field_type'] =='radio' ? 'selected' : '').'>Radio Button</option><option class="" value="file" '.($value['field_type'] =='file' ? 'selected' : '').' >File Upload</option></select></div></div>'; 
+		$i++;
 	}
 	echo '<div class="container">
 	<form >
@@ -13,7 +15,7 @@
 		</div>
 		<div class="wp_single_set_short_code">'.$add_more_db_data.'
 		</div>
-		<input type="button" id="wp_add_more" class="btn btn-default" value="Add More">
+		<input type="button"  id="wp_add_more" increment_id="" iValue='.$i.' class="btn btn-default" value="Add More">
 		<button type="submit" id="short_code_submit" class="btn btn-default">Submit</button> 
 	</form>
 </div>';
