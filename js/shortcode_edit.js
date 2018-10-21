6cfb0447e9f43b228371c9183d72abea4328fcc7
ragupathi <ragu3610@gmail.com>
2018-10-21 16:35:02 +0530

    edit Short sample Work

--- js/shortcode_edit.js
+++ js/shortcode_edit.js
@@ -1,8 +1,24 @@
 jQuery(document).ready(function(){
-	
+		jQuery("body").on("change",".wp_shot_add_shot_code_option",function(){
+			// console.log("Hello World");
+		var iValue = jQuery(this).attr('id');
+		var shot_code_option_class_name = jQuery(this).find('option:selected').attr('class');
+			jQuery('.wp_append_checkbox'+iValue).empty();
+			jQuery('.wp_append_radio'+iValue).empty();
+		if(shot_code_option_class_name =='wp_checkbox'){
+		var wp_checkbox_option = '<div class="row"><div class="form-group col-md-10"><label for="text">Checkbox Field Name</label><input type="text" class="form-control checkbox_field_name " id="text" placeholder="Enter text"></div><div class="col-md-2"><button class="Add_more_checkbox btn btn-primary" id="'+iValue+'" type="button">Add</button></div></div><div class="wp_checkbox_Addmore'+iValue+'"></div>';
+		jQuery(".wp_append_checkbox"+iValue).append(wp_checkbox_option);
+		}
+
+		if(shot_code_option_class_name =='wp_radio'){
+		var wp_radio_button_option = '<div class="row single_set_radio_button_remove"><div class="form-group col-md-10"><label for="text">Radio Button Field Name</label><input type="text" class="form-control label_name radio_button_child" id="text" placeholder="Enter text" name="label[]"></div><div class="col-md-2"><button class="add_more_radio_button btn btn-primary" id="'+iValue+'" type="button">Add</button></div></div><div class="wp_radio_button_Addmore'+iValue+'"></div>';
+		jQuery(".wp_append_radio"+iValue).append(wp_radio_button_option);
+		}
+		});
+
+
 	jQuery(".edit_short_code").click(function(){
 		var short_code_id = jQuery(this).attr("value");
-		// console.log(short_code_id);
 		jQuery.ajax({
 		type:"post",
 		url:ajaxurl,
@@ -18,14 +34,10 @@ jQuery(document).ready(function(){
 				var data = jQuery('#output').html(function(i, val) { return val*1+1 });
 				var node = document.getElementById('output');
 				var i  = node.textContent || node.innerText;
-// console.log(i);
-				// i = jQuery(this).attr("iValue");
 				// console.log(i);
 				var append_add_more = '<div class="row wp_main_short_code"><div class="form-group col-md-5"><label for="text">Label Name</label><input type="text" class="form-control label_name "   id="text" placeholder="Enter text" ></div><div class="form-group  col-md-5"><label for="exampleSelect1">Field Type</label><select class="form-control wp_shot_add_shot_code_option field_type" style="height:34px;" id="'+i+'"><option value="text" class="wp_text">Text</option><option class="wp_email" value="email">Email</option><option class="wp_password" value="password">Password</option><option class="wp_checkbox" value="checkbox">Check Box</option><option class="wp_radio" value="radio">Radio Button</option><option class="" value="file">File Upload</option></select></div><div class="col-md-2"><button type="button" class="wp_main_short_code_remove btn btn-danger">Delete</button></div><div class="wp_append_checkbox'+i+'"></div><div class="wp_append_radio'+i+'"></div>';
 			jQuery(".wp_single_set_short_code").append(append_add_more);
 		i++
-
-				// console.log("Hello World");
 			});
 
 
@@ -66,58 +78,71 @@ jQuery(document).ready(function(){
 		});
 	});
 
-		jQuery("body").on("click","#short_code_update",function(e){
+	jQuery("body").on("click","#short_code_update",function(e){
 		e.preventDefault();
-		var short_code_id = jQuery("#sample_id").val();
-		var output = [];
-		var shot_code_name = jQuery("#shot_code_name").val();
-		jQuery('.wp_main_short_code').each(function(){
-		var temp_obj = {};
-		var label_name = jQuery(this).find('.label_name').val();
-		var field_type = jQuery(this).find('.field_type').val();
-		var checkbox = jQuery(this).find('.checkbox_field_name').val();
-		var radio_button_child = jQuery(this).find('.radio_button_child');	
-		temp_obj.label_name = label_name;
-		temp_obj.field_type = field_type;
-		temp_obj.checkbox = checkbox;
-		temp_obj.radio_button = radio_button_child;
-		var checkbox_field = jQuery(this).find('.checkbox_field_name');	
-		child = [];
-		jQuery(checkbox_field).each(function(i,v){
-			var value =  jQuery(v).val();
-			child.push(value);
-			});
-		var radio_button_child = jQuery(this).find('.radio_button_child');	
-		radio_child = [];
-		jQuery(radio_button_child).each(function(i,v){
+	var short_code_id = jQuery("#sample_id").val();
+	var output = [];
+	var shot_code_name = jQuery("#shot_code_name").val();
+	jQuery('.wp_main_short_code').each(function(){
+	var temp_obj = {};
+	var label_name = jQuery(this).find('.label_name').val();
+	var field_type = jQuery(this).find('.field_type').val();
+	var checkbox = jQuery(this).find('.checkbox_field_name').val();
+	var radio_button_child = jQuery(this).find('.radio_button_child');	
+	temp_obj.label_name = label_name;
+	temp_obj.field_type = field_type;
+	temp_obj.checkbox = checkbox;
+	temp_obj.radio_button = radio_button_child;
+	var checkbox_field = jQuery(this).find('.checkbox_field_name');	
+	child = [];
+	jQuery(checkbox_field).each(function(i,v){
 		var value =  jQuery(v).val();
-		radio_child.push(value);
+		child.push(value);
 		});
-		// temp_obj.shot_code_name = shot_code_name;
-		temp_obj.radio_button = radio_child;
-		temp_obj.checkbox = child;
-		output.push(temp_obj);
-        });
-		var stringData = JSON.stringify({form_data:output,shortcode_name:shot_code_name});
-		// 	console.log(stringData);
-		jQuery.ajax({
-			type: 'post',	
-			url:ajaxurl,
-			data: {
-				action:"update_short_code_details",
-				shot_code:stringData,short_code_id:short_code_id,
-			},
-			success: function(data) {
-				// console.log(data);
-			location. reload(true);
-
-			},
-			error: function(errorThrown){
-			location. reload(true);
-
-			} 
-		});        
-        // console.log(output);
+	var radio_button_child = jQuery(this).find('.radio_button_child');	
+	radio_child = [];
+	jQuery(radio_button_child).each(function(i,v){
+	var value =  jQuery(v).val();
+	radio_child.push(value);
 	});
+	// temp_obj.shot_code_name = shot_code_name;
+	temp_obj.radio_button = radio_child;
+	temp_obj.checkbox = child;
+	output.push(temp_obj);
+    });
+	var stringData = JSON.stringify({form_data:output,shortcode_name:shot_code_name});
+	// 	console.log(stringData);
+	jQuery.ajax({
+		type: 'post',	
+		url:ajaxurl,
+		data: {
+			action:"update_short_code_details",
+			shot_code:stringData,short_code_id:short_code_id,
+		},
+		success: function(data) {
+			// console.log(data);
+		location. reload(true);
+
+		},
+		error: function(errorThrown){
+		location. reload(true);
+
+		} 
+	});        
+    // console.log(output);
+});
+
+		jQuery("body").on("click",".Add_more_checkbox",function(){
+			var Ivalue = jQuery(this).attr('id');
+			console.log(Ivalue);
+			console.log("Hello Ragupathi");
+
+			// console.log(Ivalue);
+			// var wp_checkbox_options = '<div class="row single_set_checkbox_remove add_more_checkbox"><div class="form-group col-md-10"><label for="text">Checkbox Field Name</label><input type="text" class="form-control checkbox_field_name" id="text" placeholder="Enter text" name="checkbox_field_name[]"></div><div class="col-md-2"><button class="remove_check_box btn btn-danger">Delete</button></div></div>';
+			// jQuery(".wp_checkbox_Addmore"+Ivalue).append(wp_checkbox_options);
+		});
+
+
+
 
 });
--- view/edit_short_code.php
+++ view/edit_short_code.php
@@ -7,20 +7,22 @@
 		$checkboxData = ''; $radioButtonData='';
 		if($value['field_type']=='checkbox'){
 			foreach ($value['checkbox'] as $key => $chechdat) {
-				$checkboxData=$checkboxData.'<div class="row"><div class="form-group col-md-10"><label for="text">Checkbox Field Name</label><input type="text" class="form-control checkbox_field_name " value="'.$chechdat.'" id="text" placeholder="Enter text"></div><div class="col-md-2"><button class="Add_more_checkbox btn btn-primary" id="'.$addMoreI.'" type="button">Add</button></div></div><div class="wp_checkbox_Addmore'.$addMoreI.'"></div>';
+				$checkboxData=$checkboxData.'<div class="row"><div class="form-group col-md-10"><label for="text">Checkbox Field Name</label><input type="text" class="form-control checkbox_field_name " value="'.$chechdat.'" id="text" placeholder="Enter text"></div></div>';
 			}
+			$checkboxData.='<div class="col-md-2"><button class="Add_more_checkbox btn btn-primary" id="'.$addMoreI.'" type="button">Add</button></div><div class="wp_checkbox_Addmore'.$addMoreI.'"></div>';
 			
 		}
 
 		if($value['field_type']=='radio'){
 			foreach ($value['radio_button'] as $key => $raddata) {
-			$radioButtonData = $radioButtonData.'<div class="row"><div class="form-group col-md-10"><label for="text">Radio Button Field Name</label><input type="text" class="form-control label_name radio_button_child" id="text" value="'.$raddata.'" placeholder="Enter text" name="label[]"></div><div class="col-md-2"><button class="add_more_radio_button btn btn-primary" id="'.$addMoreI.'" type="button">Add</button></div></div><div class="wp_radio_button_Addmore'.$addMoreI.'"></div>';
+			$radioButtonData = $radioButtonData.'<div class="row"><div class="form-group col-md-10"><label for="text">Radio Button Field Name</label><input type="text" class="form-control label_name radio_button_child" id="text" value="'.$raddata.'" placeholder="Enter text" name="label[]"></div></div>';
 			}
+			$radioButtonData.='<div class="col-md-2"><button class="add_more_radio_button btn btn-primary" id="'.$addMoreI.'" type="button">Add</button></div><div class="wp_radio_button_Addmore'.$addMoreI.'"></div>';
 		}
 		$addMoreI++;
 
 
-		$add_more_db_data .='<div class="row wp_main_short_code"><div class="form-group col-md-5"><label for="text">Label Name</label><input type="text" class="form-control label_name " value='.$value['label_name'].'  id="text" placeholder="Enter text" ></div><div class="form-group  col-md-5"><label for="exampleSelect1">Field Type</label><select class="form-control wp_shot_add_shot_code_option field_type"   style="height:34px;"><option value="text" '.($value['field_type'] =='text' ? 'selected' : '').'  class="wp_text">Text</option><option class="wp_email" value="email" '.($value['field_type'] =='email' ? 'selected' : '').' >Email</option><option class="wp_password" value="password" '.($value['field_type'] =='password' ? 'selected' : '').'>Password</option><option class="wp_checkbox" value="checkbox" '.($value['field_type'] =='checkbox' ? 'selected' : '').'>Check Box</option><option class="wp_radio" value="radio" '.($value['field_type'] =='radio' ? 'selected' : '').'>Radio Button</option><option class="" value="file" '.($value['field_type'] =='file' ? 'selected' : '').' >File Upload</option></select>
+		$add_more_db_data .='<div class="row wp_main_short_code"><div class="form-group col-md-5"><label for="text">Label Name</label><input type="text" class="form-control label_name " value='.$value['label_name'].'  id="text" placeholder="Enter text" ></div><div class="form-group  col-md-5"><label for="exampleSelect1">Field Type</label><select class="form-control wp_shot_add_shot_code_option field_type" id='.$i.'  style="height:34px;"><option value="text" '.($value['field_type'] =='text' ? 'selected' : '').'  class="wp_text">Text</option><option class="wp_email" value="email" '.($value['field_type'] =='email' ? 'selected' : '').' >Email</option><option class="wp_password" value="password" '.($value['field_type'] =='password' ? 'selected' : '').'>Password</option><option class="wp_checkbox" value="checkbox" '.($value['field_type'] =='checkbox' ? 'selected' : '').'>Check Box</option><option class="wp_radio" value="radio" '.($value['field_type'] =='radio' ? 'selected' : '').'>Radio Button</option><option class="" value="file" '.($value['field_type'] =='file' ? 'selected' : '').' >File Upload</option></select>
 
 
 
@@ -42,7 +44,7 @@
 		</div>
 		<div class="wp_single_set_short_code">'.$add_more_db_data.'
 		</div>
-		<div id="output" style="display:none">'.$i.'</div>
+		<div id="output" style="display:none">'.($i-1).'</div>
 		<input type="hidden" value="'.$short_code_id.'" id="sample_id">
 		<input type="button"  id="wp_add_more1" increment_id="" iValue='.$i.' class="btn btn-default" value="Add More">
 		<button type="submit" id="short_code_update" class="btn btn-default">Submit</button> 
