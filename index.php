37d088e0280e465505f1465b625faa95aaca8daa
Ragupathi <ragu3610@gmail.com>
2018-10-23 19:13:44 +0530

    wordpress commit add site transiend update plugin

--- index.php
+++ index.php
@@ -42,27 +42,27 @@ class DropboxUpload{
 		add_action('wp_ajax_edit_short_code',array($this,'edit_short_code'));
 		add_action('wp_ajax_delete_short_code',array($this,'delete_short_code'));
 		add_action('wp_ajax_update_short_code_details',array($this,'update_short_code_details'));
+		add_filter('shot-code',array($this,'shot_code_callback'),10,1);
 		add_action('wp_ajax_delete_short_code_value',array($this,'delete_short_code_value'));
 		add_action('init',array($this,'store_form_data'));
-		add_filter('shot-code',array($this,'shot_code_callback'),10,1);
 		add_filter('site_transient_update_plugins',array($this,'push_update'));
 
 	}
 
-	public function push_update($transient){
-		$plugin_slug = basename(dirname(__FILE__)).'/'.basename(__FILE__);
-		$localplugin_version =  $transient->checked[$plugin_slug];
-		// Remote Url
-		// $url = plugin_dir_url(__FILE__).'info.json';
-		$url = 'http://localhost/info.json';
-		$server_data = wp_remote_get( $url);	
+	function push_update($transient){
+		 $plugin_slug = basename(dirname(__FILE__)).'/'.basename(__FILE__);
+		 $localplugin_version =  $transient->checked[$plugin_slug];
+		// // Remote Url
+		// // $url = plugin_dir_url(__FILE__).'info.json';
+		$url = 'http://localhost/wp-form.json';
+		$server_data = wp_remote_get( $url);
 		$latest_plugin_version = json_decode($server_data['body']);
 		$server_plugin_version = $latest_plugin_version->version;
 		if($server_plugin_version >$localplugin_version){
-			$res = new stdClass();
+			$res = new DropboxUpload();
 			$res->slug = $latest_plugin_version->slug;
 			$res->new_version = $latest_plugin_version->version;
-			$res->tested = $latest_plugin_version->tested;
+			$res->plugin = $plugin_slug;
 			$res->package = $latest_plugin_version->download_url;
 			$transient->response[$plugin_slug] = $res;
 			return $transient;
