df3bca4facf3d86fab3949368ef42db9070dfd45
Ragupathi <ragu3610@gmail.com>
2018-10-25 18:51:54 +0530

    deactivation hook function call delete_options

--- index.php
+++ index.php
@@ -306,6 +306,7 @@ class DropboxUpload{
 		register_deactivation_hook( __FILE__,array($this,'deactivation_hook'));
 	}
 	public function deactivation_hook(){
+		$this->delete_options();
 		global $wpdb;
 		$table_name  = $this->db_prefix()."dropbox_details";
 		$table_name_short_code = $this->db_prefix()."custome_form";
